2d50b833a58f4a35ac7130ae13e562b57cfc1876
ahaoboy <504595380@qq.com>
2025-01-12 22:16:41 +0800

    0.0.1

---
+++ install.ps1
@@ -0,0 +1,24 @@
+$apiUrl = "https://api.github.com/repos/mpv-easy/mpv-winbuild/releases/latest"
+$extractPath = "./mpv"
+
+$response = Invoke-WebRequest -Uri $apiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$json = $response.Content | ConvertFrom-Json
+$matchingAsset = $json.assets | Where-Object { $_.name -like "mpv-x86_64-v3*.zip" }
+$url = $matchingAsset.browser_download_url
+
+$DOWNLOAD_PATH = [System.IO.Path]::GetTempFileName()+(Get-Random)+".zip"
+
+if (Test-Path -Path $tempFile) {
+  Remove-Item -Path $tempFile -Force -Recurse
+}
+
+Write-Host "download $url..."
+Invoke-WebRequest -Uri $url -OutFile $DOWNLOAD_PATH
+
+Write-Host "unzip to $extractPath"
+Expand-Archive -Path $DOWNLOAD_PATH -DestinationPath $extractPath -Force
+Write-Host "install successfully"
+
+if (Test-Path -Path $DOWNLOAD_PATH) {
+  Remove-Item -Path $DOWNLOAD_PATH -Force -Recurse
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+
+install [mpv](https://github.com/mpv-easy/mpv-winbuild/releases) by shell
+
+## windows
+
+```bash
+powershell -ExecutionPolicy Bypass -c "irm https://github.com/mpv-easy/mpv-winbuild/releases/latest/download/install.ps1 | iex"
+```
+
+## links
+- https://mpv.io/installation/
+- https://sourceforge.net/projects/mpv-player-windows/files/
+- https://github.com/shinchiro/mpv-winbuild-cmake/releases
+- https://github.com/zhongfly/mpv-winbuild/releases
+- https://github.com/mpv-easy/mpv-winbuild/releases
